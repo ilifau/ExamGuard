4d834140a6537053495cd0ce2626dd0fd8a964db
andreschweigert <andre.schweigert@ili.fau.de>
2025-05-07 11:28:21 +0200

    prevent mark with mouse

--- js/examguard.js
+++ js/examguard.js
@@ -2,6 +2,8 @@ document.addEventListener('contextmenu', function(e) { e.preventDefault(); }, fa
 document.addEventListener('keydown', function(e) {
     var isMac = navigator.platform.toUpperCase().indexOf("MAC") !== -1;
     var ctrlOrCmd = isMac ? e.metaKey : e.ctrlKey;
+    var key = e.key.toLowerCase();
+
     if (ctrlOrCmd && ["c", "v", "x", "a", "s"].includes(key)) {
         e.preventDefault();
     }
@@ -13,3 +15,7 @@ document.addEventListener('keydown', function(e) {
         e.preventDefault();
     }
 }, false);
+
+const style = document.createElement("style");
+style.innerHTML = `* { user-select: none !important; }`;
+document.head.appendChild(style);
