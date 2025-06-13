7bd377051cdcddd39d02dcff9535a9a592a5b0b1
andreschweigert <Andre.Schweigert@ili.fau.de>
2025-06-13 10:19:42 +0200

    fix call on null

--- classes/class.ilExamGuardPlugin.php
+++ classes/class.ilExamGuardPlugin.php
@@ -1,16 +1,21 @@
 <?php
-class ilExamGuardPlugin extends ilUserInterfaceHookPlugin {
+
+class ilExamGuardPlugin extends ilUserInterfaceHookPlugin
+{
     protected $config;
 
-    public function getPluginName() {
+    public function getPluginName()
+    {
         return "ExamGuard";
     }
 
-    public function hasConfiguration() {
+    public function hasConfiguration()
+    {
         return true;
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         if (!$this->config) {
             require_once __DIR__ . "/class.ilExamGuardConfig.php";
             $this->config = new ilExamGuardConfig("examguard");
@@ -18,23 +23,26 @@ class ilExamGuardPlugin extends ilUserInterfaceHookPlugin {
         return $this->config;
     }
 
-    public function modifyGUI($a_comp, $a_part, $a_par = array()) {
+    public function modifyGUI($a_comp, $a_part, $a_par = array())
+    {
         global $tpl;
 
         $cmd = $_GET["cmd"] ?? "";
-        $refid_list = $this->config->get("refid_list");
+        $refid_list = $this->getConfig()->get("refid_list");
         $refid_array = array_filter(array_map("trim", explode(",", $refid_list)));
         $current_ref_id = (int) ($_GET["ref_id"] ?? 0);
 
+        // ⏳ Verzögerung aktivieren auf "infoScreen", wenn eingestellt und ref_id passt
         if (
             $cmd === "infoScreen" &&
-            $this->config->get("start_delay") === "1" &&
+            $this->getConfig()->get("start_delay") === "1" &&
             in_array($current_ref_id, $refid_array)
         ) {
             $tpl->addJavaScript($this->getPreTestJsPath());
         }
 
-        $global_val = $this->config->get("global_block");
+        // 🧩 Copy&Paste-Block aktivieren während des Tests
+        $global_val = $this->getConfig()->get("global_block");
         $active_id = (int) ($_GET["active_id"] ?? 0);
 
         if ($global_val === "1" || ($active_id > 0 && in_array($current_ref_id, $refid_array))) {
@@ -42,11 +50,13 @@ class ilExamGuardPlugin extends ilUserInterfaceHookPlugin {
         }
     }
 
-    private function getJsPath() {
+    private function getJsPath()
+    {
         return "./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/ExamGuard/js/examguard.js";
     }
 
-    private function getPreTestJsPath() {
+    private function getPreTestJsPath()
+    {
         return "./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/ExamGuard/js/examguard_pretest.js";
     }
 }
